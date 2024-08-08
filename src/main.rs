c70e01a8634084f0c766ccc7128b78c484e33a2a
Cody Michael Jones <46515483+cm-jones@users.noreply.github.com>
2024-08-08 17:51:45 -0500

    Add main.rs

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+fn main() {
+    println!("Starting thales...");
+    
+    loop {
+        println!("Bot cycle running...");
+
+        std::thread::sleep(std::time::Duration::from_secs(5));
+    }
+}
