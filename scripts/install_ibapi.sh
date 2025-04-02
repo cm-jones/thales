3c6ac00eff72435dfd6da8dc8b54e3d9fc8500cc
Cody Michael Jones <46515483+cm-jones@users.noreply.github.com>
2025-04-02 18:37:05 -0500

    Fix script to install ibapi

--- scripts/install_ibapi.sh
+++ scripts/install_ibapi.sh
@@ -7,29 +7,28 @@ set -e
 mkdir -p third_party/ibapi
 
 # Set the IB API version - you can update this to the latest version
-IB_API_VERSION="10.19.03"
+IB_API_VERSION="1035.01"
 
 # Determine the OS type
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    OS_TYPE="linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    OS_TYPE="macos"
+if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
+    # For Linux or macOS, use the macunix package
+    DOWNLOAD_URL="https://interactivebrokers.github.io/downloads/twsapi_macunix.${IB_API_VERSION}.zip"
 else
     echo "Unsupported OS: $OSTYPE"
     exit 1
 fi
 
 # Download URL
-DOWNLOAD_URL="https://github.com/Interactive-Brokers/tws-api/archive/refs/tags/v${IB_API_VERSION}.tar.gz"
 TMP_DIR=$(mktemp -d)
 
 echo "Downloading IB API version ${IB_API_VERSION}..."
-curl -L -o "${TMP_DIR}/ibapi.tar.gz" "${DOWNLOAD_URL}"
+curl -L -o "${TMP_DIR}/ibapi.zip" "${DOWNLOAD_URL}"
 
 echo "Extracting files..."
-tar -xzf "${TMP_DIR}/ibapi.tar.gz" -C "${TMP_DIR}"
+unzip -q "${TMP_DIR}/ibapi.zip" -d "${TMP_DIR}"
 
-IB_SOURCE_DIR="${TMP_DIR}/tws-api-${IB_API_VERSION}/source/cppclient/client"
+# The C++ API client files are in the IBJts/source/cppclient/client directory
+IB_SOURCE_DIR="${TMP_DIR}/IBJts/source/cppclient/client"
 
 echo "Copying C++ API files to third_party/ibapi..."
 cp -r "${IB_SOURCE_DIR}/"* "third_party/ibapi/"
